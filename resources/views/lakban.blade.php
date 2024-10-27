466ba9801dc8e902183bd24333460119e0736e07
Rizki-AN4 <themran339@gmail.com>
2024-10-27 07:00:01 +0700

    selamat mencoba

---
+++ resources/views/lakban.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>https://pemula.com</title>
+</head>
+<body>
+  <h1 style="color:red; font-family: sans; text-aligin:center;">Heading</h1>
+  <p>Paragraf.</p>
+  <img src="logo nusra.jpg" alt="nuzra">
+</body>
+</html>
